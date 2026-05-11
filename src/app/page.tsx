3aa7c5f2a6cd56a433ca465bd70fb9ba9260f360
Jonathan <75135467+customswitch@users.noreply.github.com>
2026-05-11 16:25:36 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function HomePage() {
               marginBottom: '10px',
             }}
           >
-            GuideMaxxing
+            GuideRift
           </h1>
           <p
             style={{
