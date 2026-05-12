bc263f4442bfe4cc4e76293d4f86aa458dc01339
Jonathan <75135467+customswitch@users.noreply.github.com>
2026-05-12 18:29:10 +0200

    Update route.ts

--- src/app/admin/api/auth/route.ts
+++ src/app/admin/api/auth/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: NextRequest) {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      path: '/admin',
+      path: '/',
       maxAge: 60 * 60 * 24 * 7, // 7 days
     })
     return res
@@ -33,7 +33,7 @@ export async function DELETE() {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
-    path: '/admin',
+    path: '/',
     maxAge: 0,
   })
   return res
